8eaa64c0cf94e255780c0aa677931e981473da93
ropelletier <88781432+ropelletier@users.noreply.github.com>
2024-05-28 10:23:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ev
 
-cat cups-pdf.conf | envsubst > /etc/cups/cups-pdf.conf
-cat cupsd.conf | envsubst > /etc/cups/cupsd.conf
-cat printers.conf | envsubst > /etc/cups/printers.conf
-cat vprint.service | envsubst > /etc/avahi/services/vprint.service
-cp /usr/share/ppd/cups-pdf/CUPS-PDF_noopt.ppd /etc/cups/ppd/${PRINTER_ID}.ppd
+#cat cups-pdf.conf | envsubst > /etc/cups/cups-pdf.conf
+#cat cupsd.conf | envsubst > /etc/cups/cupsd.conf
+#cat printers.conf | envsubst > /etc/cups/printers.conf
+#cat vprint.service | envsubst > /etc/avahi/services/vprint.service
+#cp /usr/share/ppd/cups-pdf/CUPS-PDF_noopt.ppd /etc/cups/ppd/${PRINTER_ID}.ppd
 
 
 /usr/sbin/avahi-daemon --daemonize
